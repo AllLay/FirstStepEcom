2d119ba64ab831fddc7026ba40f4317f7b653cfe
AllLay <alllayisgood@gmail.com>
2025-07-09 20:28:50 +0630

    Update [...uploadthing].ts

--- src/app/api/uploadthing/[...uploadthing].ts
+++ src/app/api/uploadthing/[...uploadthing].ts
@@ -1,3 +1,18 @@
-import { uploadthingHandler } from "./core";
+import { createUploadthing, type FileRouter } from 'uploadthing/next';
 
-export { uploadthingHandler as GET, uploadthingHandler as POST };
+const ut = createUploadthing();
+
+const fileRouter: FileRouter = {
+  imageUploader: ut
+    .file()
+    .image()
+    .maxSize("4MB")
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for:", file.url);
+    }),
+};
+
+export type OurFileRouter = typeof fileRouter;
+
+export const { handler } = ut.use(fileRouter);
+export { handler as GET, handler as POST };
