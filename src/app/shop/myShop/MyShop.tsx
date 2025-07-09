fc164d10d4e243718ec57042a2c16c80080de25c
AllLay <alllayisgood@gmail.com>
2025-07-09 20:44:23 +0630

    Update MyShop.tsx

--- src/app/shop/myShop/MyShop.tsx
+++ src/app/shop/myShop/MyShop.tsx
@@ -13,7 +13,7 @@ import {
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useUploadThing } from "@uploadthing/react";
+import { useUploadThing } from "uploadthing/client";
 
 interface Product {
   _id: string;
