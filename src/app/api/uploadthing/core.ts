afb66d168734a4d43394bde7290af7f08e32123b
AllLay <alllayisgood@gmail.com>
2025-07-09 21:22:00 +0630

    Update core.ts

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -4,8 +4,8 @@ const f = createUploadthing();
 
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
-    .onUploadComplete(async ({ file }) => {
-      console.log("Upload complete:", file.url);
+    .onUploadComplete(({ file }) => {
+      console.log("Upload complete", file.url);
     }),
 } satisfies FileRouter;
 
