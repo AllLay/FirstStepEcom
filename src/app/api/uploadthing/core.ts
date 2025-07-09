8b8b06b3fb08bb040809b017e0b26fa022ef5cbf
AllLay <alllayisgood@gmail.com>
2025-07-09 21:26:45 +0630

    Update core.ts

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,9 +3,9 @@ import { createUploadthing, type FileRouter } from "uploadthing/server";
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  productImage: f({ image: { maxFileSize: "4MB" } })
     .onUploadComplete(({ file }) => {
-      console.log("Upload complete", file.url);
+      console.log("Upload completed:", file.url);
     }),
 } satisfies FileRouter;
 
