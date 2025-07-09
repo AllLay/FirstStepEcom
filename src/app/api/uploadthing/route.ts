d3c8100e76305615988804d5e8a534f5e5954696
AllLay <alllayisgood@gmail.com>
2025-07-09 19:00:13 +0630

    Update route.ts

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,6 +1,6 @@
-import { createNextRouteHandler } from 'uploadthing/next';
+import { uploadthing } from 'uploadthing/next';
 import { ourFileRouter } from './core';
 
-export const { GET, POST } = createNextRouteHandler({
-  router: ourFileRouter,
-});
+const handler = uploadthing(ourFileRouter);
+
+export const { GET, POST } = handler;
