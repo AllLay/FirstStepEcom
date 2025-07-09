159a65cea30a050468659c22d0ac64008d5f5e45
AllLay <alllayisgood@gmail.com>
2025-07-09 19:11:01 +0630

    Update FileUpload.tsx

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { useUploadThing, type UploadFile } from 'uploadthing/react';
+import { useUploadThing, type UploadFile } from 'uploadthing/next';
 import { Button } from 'your-ui-library-or-html';
 
 interface FileUploadProps {
