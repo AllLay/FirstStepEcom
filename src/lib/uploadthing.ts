ba6d8c04e26bfdbf31ababa80b0d7e0bb085dd48
AllLay <alllayisgood@gmail.com>
2025-07-09 20:19:16 +0630

    Update uploadthing.ts

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,21 +1,4 @@
-import { generateUploadThingURL } from 'uploadthing/client';
-import { FileRouter } from '@/app/api/uploadthing/core';
+import { generateReactHelpers } from '@uploadthing/react/hooks';
+import type { OurFileRouter } from '@/app/api/uploadthing/core';
 
-export const uploadFiles = async (
-  endpoint: keyof FileRouter,
-  { files }: { files: File[] }
-) => {
-  const url = generateUploadThingURL(`/api/uploadthing`, endpoint);
-  const formData = new FormData();
-
-  files.forEach((file) => formData.append('files', file));
-
-  const res = await fetch(url, {
-    method: 'POST',
-    body: formData,
-  });
-
-  if (!res.ok) throw new Error('Upload failed');
-
-  return res.json();
-};
+export const { uploadFiles } = generateReactHelpers<OurFileRouter>();
