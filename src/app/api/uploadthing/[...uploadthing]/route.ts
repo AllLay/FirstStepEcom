ba5b9afde598cc0d57cf13bbad35223751be8a7d
AllLay <alllayisgood@gmail.com>
2025-07-09 21:43:35 +0630

    Update route.ts

--- src/app/api/uploadthing/[...uploadthing]/route.ts
+++ src/app/api/uploadthing/[...uploadthing]/route.ts
@@ -1,9 +1,11 @@
-import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { createUploadthing } from "uploadthing/next";
 
 const f = createUploadthing();
 
-export const uploadRouter: FileRouter = {
-  productImage: f.image({ maxFileCount: 1 }),
+console.log("f is", f);
+
+export const uploadRouter = {
+  imageUploader: f.image({ maxFileCount: 1 }),
 };
 
 const handler = f.handleUpload(uploadRouter);
