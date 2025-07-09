b6e03f48c414cc593c97b795cd84c594c4f996f3
AllLay <alllayisgood@gmail.com>
2025-07-09 19:22:00 +0630

    Update uploadthing.ts

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,7 +1,6 @@
-import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { createUploadthing, type FileRouter } from 'uploadthing/next';
 
 const f = createUploadthing();
-
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB" } }),
+  imageUploader: f({ image: { maxFileSize: '4MB' } }),
 } satisfies FileRouter;
