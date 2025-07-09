567ede80f0860e0e97f295cb66ed730fde421621
AllLay <alllayisgood@gmail.com>
2025-07-09 22:01:48 +0630

    Update route.ts

--- src/app/api/uploadthing/[...uploadthing]/route.ts
+++ src/app/api/uploadthing/[...uploadthing]/route.ts
@@ -1,17 +1,9 @@
-import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { createRouteHandler } from "uploadthing/next";
-
-const f = createUploadthing();
-
-export const uploadRouter: FileRouter = {
-  productImage: f({
-    image: { maxFileSize: "4MB", maxFileCount: 1 },
-  }),
-};
+import { uploadRouter } from "@/lib/uploadthing";
 
 export const { GET, POST } = createRouteHandler({
   router: uploadRouter,
   config: {
-    uploadthingSecret: "sk_live_7e276cea24bb8f26d4803e9e0f738bee5efaff401d83e3079c2c6a4d36f7600f",
+    uploadthingSecret: process.env.UPLOADTHING_SECRET || "",
   },
 });
