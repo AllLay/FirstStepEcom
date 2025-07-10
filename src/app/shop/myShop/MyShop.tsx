02fe06c54545329f7f31b87cd5d1fd69d78b8898
AllLay <alllayisgood@gmail.com>
2025-07-10 21:41:05 +0630

    Update MyShop.tsx

--- src/app/shop/myShop/MyShop.tsx
+++ src/app/shop/myShop/MyShop.tsx
@@ -95,7 +95,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
 };
 
 function MyShop() {
-  const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
+  const API_BASE = process.env.NEXT_PUBLIC_API_URL;
 
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
   const [openPopup, setOpenPopup] = useState(false);
