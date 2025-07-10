fddbc032c896e2940d35afbf42992763bdbf0453
AllLay <alllayisgood@gmail.com>
2025-07-10 14:25:08 +0630

    Delete src/lib/uploadthing.ts

--- src/lib/uploadthing.ts
+++
@@ -1,9 +0,0 @@
-import { createUploadthing, type FileRouter } from "uploadthing/next";
-
-const f = createUploadthing();
-
-export const uploadRouter = {
-  productImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } }),
-} satisfies FileRouter;
-
-export type OurFileRouter = typeof uploadRouter;
