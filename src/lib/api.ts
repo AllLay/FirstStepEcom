ed3ff888ef02004ad60a31ea9dfa627469ce7ce8
AllLay <alllayisgood@gmail.com>
2025-07-10 20:59:25 +0630

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://firststepecom-b.onrender.com/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://firststepecom-b.onrender.com',
   withCredentials: true,
 });
 
