3e92c61d02816e7f7b8bef2ec7ba6a76b004644d
AllLay <alllayisgood@gmail.com>
2025-07-10 14:25:19 +0630

    Delete src/app/api/uploadthing directory

--- src/app/api/uploadthing/route.ts
+++
@@ -1,6 +0,0 @@
-import { createNextRouteHandler } from "uploadthing/next";
-import { ourFileRouter } from "@/lib/uploadthing";
-
-export const { GET, POST } = createNextRouteHandler({
-  router: ourFileRouter,
-});
